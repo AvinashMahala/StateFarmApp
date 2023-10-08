28c313a435caa34200197abe0dfaab0d622f893a
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-10-08 06:35:15 -0500

    Update DashboardScreen.js

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -29,150 +29,151 @@ const DashboardScreen = () => {
 
   return (
     <SafeAreaView
-      style={{ flex: 1, backgroundColor: "white", alignItems: "center" ,
-    height:1300,
-    flexDirection:'column',
-    justifyContent:'space-around',
-    overflow:'scroll',
-  
-    }}
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        height: 1300,
+        flexDirection: "column",
+        justifyContent: "space-around",
+        overflow: "scroll",
+      }}
     >
-      <View style={styles.child11} 
-      
-      >
-        
-        <Text style={styles.title}>
-        Basic Plan
-        </Text>
+
+<View style={styles.header}>
+        <Text style={styles.headerText}>Dashboard</Text>
+      </View>
+
+      <View style={styles.body}>
+        <View style={styles.child11}>
+        <Text style={styles.title}>Basic Plan</Text>
         <View style={styles.body}>
-          <Text style={styles.pricing}>
-            {"Pricing  "}
-          </Text>
+          <Text style={styles.pricing}>{"Pricing  "}</Text>
           {/* <Text style={styles.subtit}>
             {"Basic and Essential Coverage"}
           </Text> */}
           <Text style={styles.desc}>
             {"This Plan covers the most essential features"}
           </Text>
-           <Text style={styles.desc}>
-            {`Maintainance `}
-          </Text>
-
+          <Text style={styles.desc}>{`Maintainance `}</Text>
         </View>
-      
-        </View>
-        <View style={styles.child11} >
-        
-        <Text style={styles.title}>
-        Recommended    {"\t\t\tPlan"}
-        </Text>
+      </View>
+      <View style={styles.child11}>
+        <Text style={styles.title}>Recommended {"\t\t\tPlan"}</Text>
         <View style={styles.body}>
-          <Text style={styles.pricing}>
-            {"Pricing  "}
-          </Text>
+          <Text style={styles.pricing}>{"Pricing  "}</Text>
           {/* <Text style={styles.subtit}>
             {"Basic and Essential Coverage"}
           </Text> */}
           <Text style={styles.desc}>
             {"This Plan provides covers the recommended features"}
           </Text>
-          <Text style={styles.desc}>
-            {`Maintainance `}
-          </Text>
-
+          <Text style={styles.desc}>{`Maintainance `}</Text>
         </View>
-      
-        </View>
-        <View style={styles.child11} >
-        
-        <Text style={styles.title}>
-        Basic Plan
-        </Text>
+      </View>
+      <View style={styles.child11}>
+        <Text style={styles.title}>Basic Plan</Text>
         <View style={styles.body}>
-          <Text style={styles.pricing}>
-            {"Pricing  "}
-          </Text>
+          <Text style={styles.pricing}>{"Pricing  "}</Text>
           {/* <Text style={styles.subtit}>
             {"Basic and Essential Coverage"}
           </Text> */}
-          <Text style={styles.desc}>
-            {"This Plan only offers the best"}
-          </Text>
-          <Text style={styles.desc}>
-            {`Maintainance `}
-          </Text>
-
+          <Text style={styles.desc}>{"This Plan only offers the best"}</Text>
+          <Text style={styles.desc}>{`Maintainance `}</Text>
         </View>
+      </View>
+      </View>
       
-        </View>
-        
-       
+      <View style={styles.footer}>
+        <Pressable
+          onPress={() => navigation.navigate("Homepage")}
+          style={styles.homeButton}
+        >
+          <Text style={styles.homeButtonText}>Go to Homepage</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    logoutButton: {
-        marginTop: 20,
-        padding: 15,
-        backgroundColor: "#FF0000",
-        borderRadius: 5
-    },
-    logoutText: {
-        color: "white",
-        fontWeight: "bold"
-    },
-    child11:{
-        height:180,
-      marginTop:80,
-      width:300,
-     
-      display:'flex',
-      flexDirection:'column',
-      justifyContent:'space-around',
-      alignItems:'center',
-      borderRadius : 9,
-      shadowColor: "black",
-      shadowOffset: { width: 10, height: 10 },
-      shadowOpacity: 0.5,
-      shadowRadius: 5,
-      elevation: 3,
-      backgroundColor:'blue',
-    },
-    title:{
-      fontSize:40,
-      color:'white',
-      
-    },
-    body:{
-      
-      color:'white',
-      display:'flex',
-      flexDirection:'column',
-      justifyContent:'space-around',
-      alignItems:'center',
-    },
-    pricing:{
-        fontSize:25,
-        color:'white',
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoutButton: {
+    marginTop: 20,
+    padding: 15,
+    backgroundColor: "#FF0000",
+    borderRadius: 5,
+  },
+  logoutText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  child11: {
+    height: 180,
+    marginTop: 80,
+    width: 300,
 
-    },
-    subtit:{
-      fontSize:20,
-      color:'white',
-      textAlign:'center'
-    },
-    desc:{
-      fontSize:17,
-      color:'white',
-      maxWidth: 270,
-    }
-
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderRadius: 9,
+    shadowColor: "black",
+    shadowOffset: { width: 10, height: 10 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 3,
+    backgroundColor: "blue",
+  },
+  title: {
+    fontSize: 40,
+    color: "white",
+  },
+  body: {
+    color: "white",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  pricing: {
+    fontSize: 25,
+    color: "white",
+  },
+  subtit: {
+    fontSize: 20,
+    color: "white",
+    textAlign: "center",
+  },
+  desc: {
+    fontSize: 17,
+    color: "white",
+    maxWidth: 270,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    padding: 15,
+    backgroundColor: 'white',
+    borderTopWidth: 1,
+    borderTopColor: '#e5e5e5',
+    alignItems: 'center'
+  },
+  homeButton: {
+    padding: 10,
+    backgroundColor: 'blue',
+    borderRadius: 5,
+  },
+  homeButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16
+  }  
 });
 
 export default DashboardScreen;
